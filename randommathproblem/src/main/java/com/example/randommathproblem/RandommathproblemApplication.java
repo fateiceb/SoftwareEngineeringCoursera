a8932d70205873db0dbab14edb9c350e80c0da60
fateiceb <993661862@qq.com>
2018-03-10 16:03:41 +0800

    添加readme

---
+++ randommathproblem/src/main/java/com/example/randommathproblem/RandommathproblemApplication.java
@@ -0,0 +1,12 @@
+package com.example.randommathproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandommathproblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandommathproblemApplication.class, args);
+	}
+}
